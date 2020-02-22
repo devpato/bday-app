1b7f2c217428eac7c7a3bf226e9ebdc61e29b0bc
devpato <pevargas2730@gmail.com>
2020-02-22 03:05:40 -0500

    added common module

--- src/app/bday-page/bday.module.ts
+++ src/app/bday-page/bday.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BdayShellComponent } from './components/bday-shell/bday-shell.component';
 import { BdayFormComponent } from './containers/bday-form/bday-form.component';
+import { CommonModule } from '@angular/common';
 
 const bdayRoutes: Routes = [{ path: "", component: BdayShellComponent }];
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild(bdayRoutes),
   ],
   declarations: [
