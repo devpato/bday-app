7c54040a69c159a75d730acb033caaf8001ce883
Patricio Vargas <patriciovargas@Patricios-MacBook-Pro.local>
2020-02-22 01:26:02 -0500

    bday module

---
+++ src/app/bday-page/bday.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const bdayRoutes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(bdayRoutes),
+  ],
+  declarations: [
+  ]
+})
+export class ProductModule {}
