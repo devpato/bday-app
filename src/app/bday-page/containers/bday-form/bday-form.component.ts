9f863a6219b101e25cd124e6be15f7acb32e8f4b
devpato <pevargas2730@gmail.com>
2020-02-22 03:10:00 -0500

    dummy data structure

--- src/app/bday-page/containers/bday-form/bday-form.component.ts
+++ src/app/bday-page/containers/bday-form/bday-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bday-form',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bday-form.component.scss']
 })
 export class BdayFormComponent implements OnInit {
+  question: Observable<string>;
+  month: string
 
+  questions = [
+    `What is your name?`,
+    `What is your birth month?`,
+    `What day in ${this.month} were you born?`,
+    `What to add an other birth day`
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
