5d1bae7f0dfdb294717b46bda75ee93bdca6a4d1
devpato <pevargas2730@gmail.com>
2020-02-22 14:35:09 -0500

    fixed feature store module name

--- src/app/bday-page/bday.module.ts
+++ src/app/bday-page/bday.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 import { BdayShellComponent } from './components/bday-shell/bday-shell.component';
 import { BdayFormComponent } from './containers/bday-form/bday-form.component';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from "@ngrx/store";
+import { reducer } from './state/bday.reducer';
 
 const bdayRoutes: Routes = [{ path: "", component: BdayShellComponent }];
 
@@ -10,6 +12,7 @@ const bdayRoutes: Routes = [{ path: "", component: BdayShellComponent }];
   imports: [
     CommonModule,
     RouterModule.forChild(bdayRoutes),
+    StoreModule.forFeature("bday", reducer)
   ],
   declarations: [
   BdayShellComponent,
