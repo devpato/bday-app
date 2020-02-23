ea58c919c219d897a4b79efd27bb0a0296a90d6c
devpato <pevargas2730@gmail.com>
2020-02-23 03:04:19 -0500

    service pulling json

--- src/app/bday-page/bday.service.ts
+++ src/app/bday-page/bday.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Question } from './models/question.model';
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BdayService {
 
-  constructor() { }
+  private _jsonURL = 'assets/questions-data.json';
+
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>(this._jsonURL).pipe(catchError(this.handleError));
+  }
+
+  private handleError(err) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    return throwError(errorMessage);
+  }
 }
