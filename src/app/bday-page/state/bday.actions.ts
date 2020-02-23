164659c02c4306c11ae335ba9c4da6cbfa168bce
devpato <pevargas2730@gmail.com>
2020-02-23 03:10:29 -0500

    added more actions

--- src/app/bday-page/state/bday.actions.ts
+++ src/app/bday-page/state/bday.actions.ts
@@ -2,10 +2,31 @@ import { Action } from "@ngrx/store";
 import { Question } from "../models/question.model";
 
 export enum BdayActionTypes {
-  SetCurrentQuestion = "[Question] Set Next Cuestion",
-  LoadCurrentQuestion = "[Question] Load Success",
-  SaveQuestion = '[Question] Save Question',
-  AddNewBday = '[Question] Add New Bday',
+  Load = '[Bday] Load Questions',
+  SuccessLoadQuestions = '[Bday] Load Sucess Questions',
+  FailLoadQuestions = '[Bday] Load Fail Questions',
+  SetCurrentQuestion = "[Bday] Set Next Question",
+  LoadCurrentQuestion = "[Bday] Load Success",
+  SaveQuestion = '[Bday] Save Question',
+  AddNewBday = '[Bday] Add New Bday',
+}
+
+export class LoadQuestions implements Action {
+  readonly type = BdayActionTypes.Load;
+}
+
+
+export class SuccessLoadQuestions implements Action {
+  readonly type = BdayActionTypes.SuccessLoadQuestions;
+
+  constructor(public payload: Question[]) { }
+}
+
+
+export class FailLoadQuestions implements Action {
+  readonly type = BdayActionTypes.FailLoadQuestions;
+
+  constructor(public payload: string) { }
 }
 
 export class SetCurrentQuestion implements Action {
@@ -30,6 +51,9 @@ export class AddNewBay implements Action {
 
 
 export type BdayActions =
+  | LoadQuestions
+  | SuccessLoadQuestions
+  | FailLoadQuestions
   | SetCurrentQuestion
   | LoadCurrentQuestion
   | SaveQuestion
