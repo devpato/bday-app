e48268fc47937f57754ceee1d70682459fb06485
devpato <pevargas2730@gmail.com>
2020-02-24 15:44:56 -0500

    shared module

--- src/app/bday-page/bday.module.ts
+++ src/app/bday-page/bday.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BdayShellComponent } from './components/bday-shell/bday-shell.component';
 import { BdayFormComponent } from './containers/bday-form/bday-form.component';
-import { CommonModule } from '@angular/common';
 import { StoreModule } from "@ngrx/store";
 import { reducer } from './state/bday.reducer';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
 import { EffectsModule } from "@ngrx/effects";
 import { BdayEffects } from './state/bday.effect';
 import { BdayResultsComponent } from './containers/bday-results/bday-results.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -17,11 +15,8 @@ const bdayRoutes: Routes = [{ path: "", component: BdayShellComponent }];
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule.forChild(bdayRoutes),
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    SharedModule,
     StoreModule.forFeature("bday", reducer),
     EffectsModule.forFeature([BdayEffects])
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
-  imports: [
-  ],
-  declarations: [
+  imports: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
-export class SharedModule {}
+export class SharedModule { }
